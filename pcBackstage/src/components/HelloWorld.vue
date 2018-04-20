f954a52a178bdf30d329cc0862860749d0b22f4e
KaXi333 <960178541@qq.com>
2018-04-20 11:28:35 +0800

    第一次提交

---
+++ pcBackstage/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-continer">
+    <div class="login-bc"></div>
+    <div class="login-border">
+      <h1>
+        <strong>后台管理系统</strong>
+        <em>Management System</em>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{
+  },
+  data () {
+      return {
+       
+    } 
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
